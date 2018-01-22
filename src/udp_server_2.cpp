d227f74ed65ec2d462360e26cff41593979d6671
skramm <sebastien.kramm@univ-rouen.fr>
2018-01-22 18:50:52 +0100

    problem: bind with std::function

--- src/udp_server_2.cpp
+++ src/udp_server_2.cpp
@@ -16,6 +16,7 @@
 #define DO_SEND_ACK
 
 using boost::asio::ip::udp;
+typedef std::function< void( const boost::system::error_code&, std::size_t bytes_rx, int ) > CALLBACK_FUNCTION_T;
 
 //-----------------------------------------------------------------------------------
 class my_udp_server
@@ -24,7 +25,6 @@ class my_udp_server
 		my_udp_server( boost::asio::io_service& io_service, int port_no, bool sendack=false )
 			: _socket( io_service, udp::endpoint( udp::v4(), port_no ) ), _sendack(sendack)
 		{
-//			start_receive();
 		}
 
 		void start_receive()
@@ -32,68 +32,42 @@ class my_udp_server
 			_socket.async_receive_from(
 				boost::asio::buffer( _recv_buffer),
 				_remote_endpoint,
-#if 0
+#if 1
 				boost::bind(
 					my_udp_server::_rx_handler,
 					this,
 					boost::asio::placeholders::error,
-					boost::asio::placeholders::bytes_transferred
+					boost::asio::placeholders::bytes_transferred,
+					42
 				)
 #else
 				my_udp_server::_rx_handler
 #endif
 			);
 		}
+		void assignCallback( CALLBACK_FUNCTION_T f )
+		{
+			_rx_handler = f;
+		}
+
 	private:
 
-/*	void handler_rx( const boost::system::error_code& error, std::size_t bytes_rx )
-	{
-		static int iter(1);
-		std::cout << "rx handler(), call " << iter++ << "\n";
-		if( !error || error == boost::asio::error::message_size )
-		{
-			std::cout << "bt=" << bytes_rx << " bytes, data=*";
-			std::cout.write( _recv_buffer.data(), bytes_rx );
-			std::cout << "*\n";
-
-			if( _sendack )
-			{
-				std::string str( "ok\n" );
-#if 0
-				_socket.async_send_to(          // asynchronous send acknowledge
-					boost::asio::buffer(str),
-					_remote_endpoint,
-					handler_tx
-				);
-#else
-				std::cout << "sending ack\n";
-				_socket.send_to(                // synchronous send acknowledge
-					boost::asio::buffer(str),
-					_remote_endpoint
-				);
-#endif
-			}
-			start_receive();
-		}
-		else
-			std::cout << "error on receive\n";
-	}*/
-
-	udp::socket   _socket;
-	udp::endpoint _remote_endpoint;
-	boost::array<char, 1024> _recv_buffer;
-	bool _sendack;
-	std::function< void(const boost::system::error_code&, std::size_t bytes_rx ) > _rx_handler;
+		udp::socket              _socket;
+		udp::endpoint            _remote_endpoint;
+		boost::array<char, 1024> _recv_buffer;
+		bool                     _sendack;
+		CALLBACK_FUNCTION_T      _rx_handler;
 };
 
 //-----------------------------------------------------------------------------------
 void
-my_rx_handler( const boost::system::error_code&, std::size_t bytes_rx )
+my_rx_handler( const boost::system::error_code& error, std::size_t bytes_rx, int third )
 {
-	std::cout << "my_rx_handler()\n";
+	std::cout << "my_rx_handler() !\n";
 }
 
-int main( int argc, const char** argv )
+int
+main( int argc, const char** argv )
 {
 	bool sendack(false);
 	if( argc > 1 )
@@ -109,6 +83,8 @@ int main( int argc, const char** argv )
 
 		my_udp_server server( io_service, 12345, sendack );
 		std::cout << "server created\n";
+
+		server.assignCallback( my_rx_handler );
 
 		server.start_receive();
 		std::cout << "server started\n";
